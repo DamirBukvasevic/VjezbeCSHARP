b8f6e9e785eac0e3efa9416ea72a42ca630fea8a
DamirBukvasevic <damirbukvasevic@gmail.com>
2024-06-23 21:10:20 +0200

    Debug greška, popravak

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,6 +1,9 @@
 ﻿using UcenjeCS;
 
-E05Z2.Izvedi();
+
+
+
+//E05Z2.Izvedi();
 //E05Z1.Izvedi();
 //E05Nizovi.Izvedi();
 //E04Z1.Izvedi();
@@ -8,10 +11,7 @@ E05Z2.Izvedi();
 //E03Z3.Izvedi();
 //E03Z2.Izvedi();
 //E03Z1.Izvedi();
-//E01Z3.Izvedi();
-//E01Z2.Izvedi();
-//E01Z1.Izvedi();
 //E02VarijableTipoviPodatakaOperatori.Izvedi();
+E01Z2.Izvedi();
+//E01Z1.Izvedi();
 //E01UlazIzlaz.Izvedi();
-
-
