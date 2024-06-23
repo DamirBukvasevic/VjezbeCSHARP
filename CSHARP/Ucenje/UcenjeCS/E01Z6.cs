e36601adffd5e6cc31f90b3746c8bf6787ee17bb
DamirBukvasevic <damirbukvasevic@gmail.com>
2024-06-23 21:25:17 +0200

    E01Z6 Rjesenje

---
+++ CSHARP/Ucenje/UcenjeCS/E01Z6.cs
@@ -0,0 +1,19 @@
+﻿namespace UcenjeCS
+{
+    internal class E01Z6
+    {
+        public static void Izvedi()
+        {
+            //Program od korisnika učitava ime grada i broj stanovnika.
+            //Ispisuje rečenicu: U XXXXXXX živi XXXXX ljudi.
+
+            Console.Write("Unesite grad: ");
+            string grad = Console.ReadLine();
+
+            Console.Write("Unesite broj stanovnika: ");
+            int brojS = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("U {0} Živi {1} Stanovnika", grad, brojS);
+        }
+    }
+}
